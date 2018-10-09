60721d5d4ccbc56e76ac908d18cb935e2df41873
gzamfire <gzamfire@gmail.com>
2018-10-09 01:27:47 +0000

    Update die.ts

--- die.ts
+++ die.ts
@@ -1,10 +1,12 @@
 const magik = magikcraft.io;
 
 function die() {
-    let me = magik.hic();
-    me.setX(-173);
-    me.setY(60);
-    me.setZ(245);
+    var me = magik.hic();
+    // me.setX(-173);
+    // me.setY(60);
+    // me.setZ(245);
+
+    me.setY(-150);
 
     magik.ianuae(me);
 }
