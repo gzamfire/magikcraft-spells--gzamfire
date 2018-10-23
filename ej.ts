9b7e66dd4d5094c53ccbfd85206b6b2d02e398c1
gzamfire <gzamfire@gmail.com>
2018-10-23 01:13:50 +0000

    Update ej.ts

--- ej.ts
+++ ej.ts
@@ -1,11 +1,11 @@
 const magik = magikcraft.io;
 
 function eject() {
-    magik.iacta("lazyturtle99_");
-    magik.type("material").Ladder();
+    // magik.iacta("lazyturtle99_");
+    var ladder = magik.type("material").Ladder();
     
-    magik.dixit("ejected");
-    magik.dixit(magik.hic().toString());
+    // magik.dixit("ejected");
+    magik.dixit(ladder.toString());
 }
 
 eject();
