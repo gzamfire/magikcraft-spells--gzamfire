d68d2d0832a2ecce6960d9443c504f5fad882074
gzamfire <gzamfire@gmail.com>
2018-11-06 02:10:40 +0000

    Update w2.ts

--- w2.ts
+++ w2.ts
@@ -96,7 +96,7 @@ function wall2() {
     // zD range -5 to 5 (height of wall)
     // xD range -5 to 5 (width of wall)
 
-    for (let zD = -1 * wallWidth; wallWidth <= 5; zD++) {
+    for (let zD = -1 * wallWidth; zD <= 5; zD++) {
                 
         for (let xD = -1 * wallWidth; xD <= wallWidth; xD++) {
             
