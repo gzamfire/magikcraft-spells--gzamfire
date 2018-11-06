2814f22226fd1b4cee68fd8b8248e090a8178f27
gzamfire <gzamfire@gmail.com>
2018-11-06 01:38:08 +0000

    Update w2.ts

--- w2.ts
+++ w2.ts
@@ -92,28 +92,28 @@ function wall2() {
 
     /* BUILD THE ROOF */
 
-    // let yD = roofHeight;
-    // // zD range -5 to 5 (height of wall)
-    // // xD range -5 to 5 (width of wall)
+    yD = roofHeight;
+    // zD range -5 to 5 (height of wall)
+    // xD range -5 to 5 (width of wall)
 
-    // for (let zD = -1 * wallWidth; wallWidth <= 5; zD++) {
+    for (let zD = -1 * wallWidth; wallWidth <= 5; zD++) {
                 
-    //     for (let xD = -1 * wallWidth; xD <= wallWidth; xD++) {
+        for (let xD = -1 * wallWidth; xD <= wallWidth; xD++) {
             
-    //         let me = magik.hic();
-    //         me.setX(me.getX() + xD);
-    //         me.setY(me.getY() + yD);
-    //         me.setZ(me.getZ() + zD);
+            let me = magik.hic();
+            me.setX(me.getX() + xD);
+            me.setY(me.getY() + yD);
+            me.setZ(me.getZ() + zD);
 
-    //         me.getBlock().setType(targetType);
-    //         // clearCount++;
+            me.getBlock().setType(targetType);
+            // clearCount++;
 
-    //     }
-    // }
+        }
+    }
 
     /* BUILD THE FLOOR */
 
-    let yD = floorHeight;
+    yD = floorHeight;
     // zD range -5 to 5 (height of wall)
     // xD range -5 to 5 (width of wall)
 
