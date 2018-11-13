1d04bcae28d7d568c490f9e90a5be53ccf1fef71
gzamfire <gzamfire@gmail.com>
2018-11-13 01:25:29 +0000

    Update w2.ts

--- w2.ts
+++ w2.ts
@@ -14,7 +14,7 @@ function wall2() {
     const wallWidth = 3;
 
     let teleportPoint = magik.hic();
-    teleportPoint.setY(me.getY() + 4);
+    teleportPoint.setY(me.getY() + 6);
     magik.ianuae(teleportPoint);
 
     let xD = wallWidth;
