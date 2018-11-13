f3309a7047d2b42d4236948871d34997c3695b02
gzamfire <gzamfire@gmail.com>
2018-11-13 02:01:25 +0000

    Update w2.ts

--- w2.ts
+++ w2.ts
@@ -7,7 +7,7 @@ function wall2() {
 
     const Material = magik.type('Material');
 
-    let targetType = Material.GLASS;
+    let targetType = Material.LADDER;
     let air = Material.AIR;
     const roofHeight = 3 + 4;
     const floorHeight = -2 + 4;
