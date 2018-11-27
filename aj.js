41cb332222dcb02a017e5e552da16775e32472f3
gzamfire <gzamfire@gmail.com>
2018-11-27 21:54:06 +0000

    Update aj.js

--- aj.js
+++ aj.js
@@ -9,7 +9,7 @@ function airJordans() {
         let me = magik.hic();
         me.setY(me.getY() - 1);
         me.getBlock().setType(Material.GLASS);
-
+        magik.dixit("Air.")
     }, 100);
 
 
