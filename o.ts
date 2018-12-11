84c747f942d9fff4974d20ec0f0337f8ee404cc4
gzamfire <gzamfire@gmail.com>
2018-12-11 02:23:43 +0000

    Update o.ts

--- o.ts
+++ o.ts
@@ -1,10 +1,17 @@
 const magik = magikcraft.io;
 
+function randRange(lower, higher) {
+    return parseInt(Math.random() * (higher - lower) + lower);
+}
+
 function ori() {
     let me = magik.hic();
-    me.setX(-69);
+
+    let randX = randRange(-500, 500);
+    let randZ = randRange(-500, 500);
+    me.setX(randX);
     me.setY(63);
-    me.setZ(330);
+    me.setZ(randZ);
 
     magik.ianuae(me);
 }
