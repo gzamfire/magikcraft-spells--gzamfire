751403c5f552733f5e2d75b9984ac3ab1143b097
gzamfire <gzamfire@gmail.com>
2018-12-11 02:07:40 +0000

    Update o.ts

--- o.ts
+++ o.ts
@@ -2,9 +2,9 @@ const magik = magikcraft.io;
 
 function ori() {
     let me = magik.hic();
-    me.setX(-173);
-    me.setY(67);
-    me.setZ(245);
+    me.setX(-69);
+    me.setY(63);
+    me.setZ(330);
 
     magik.ianuae(me);
 }
