9abb42db473c25639d5bf033241bb287120a992c
gzamfire <gzamfire@gmail.com>
2018-12-11 01:31:07 +0000

    Update podium.ts

---
+++ podium.ts
@@ -0,0 +1,19 @@
+const magik = magikcraft.io;
+
+let count = 0;
+const Material = magik.type('Material');
+
+function podium() {
+
+    let me = magik.hic();
+
+    magik.doNTimes(() => {
+        me.setY(me.getY() + 1);
+        me.getBlock().setType(Material.GLASS);
+    }, 50, 0);
+
+    me.setY(me.getY() + 1);
+    magik.ianuae(me);
+
+
+}
