5195b58921d120648b1a90a56ab83feaf325a774
gzamfire <gzamfire@gmail.com>
2018-12-11 02:32:31 +0000

    Update go.ts

--- go.ts
+++ go.ts
@@ -3,7 +3,7 @@ const magik = magikcraft.io;
 function goTo() {
     let sender = magik.getPlugin().getServer().getPlayer("bravergragoon77");
 
-    let loc = sender.getEyeLocation();//.getDirection();
+    let loc = sender.getEyeLocation().getDirection();
     magik.dixit(loc.toString());
     
     magik.ianuae(loc);
